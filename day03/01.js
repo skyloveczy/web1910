84f6b25cc0dbb0f7b2f4fbdf7414ece17967836d
shen <123456@qq.com>
2020-02-14 17:19:00 +0800

    第三天内容结束
    上午github的使用,下午ssh配置,nodejs入门

---
+++ day03/01.js
@@ -0,0 +1,9 @@
+console.log("脱离浏览器运行js");
+
+// alert(111); // 报错,alert是浏览器对象中的方法
+
+// console.log(document); // 报错,document也是浏览器对象中的
+
+// confirm("111");
+
+// console.log(localStorage);
---
+++ day03/02_server.js
@@ -0,0 +1,17 @@
+// 1 引入http
+var http = require("http");
+
+// 2 创建服务 req为请求,res为响应
+var server = http.createServer(function(req,res){
+  // 4 返回响应给浏览器
+  // end的作用: 结束请求,返回响应
+  // res.end("<h1 style='color:red;'>This is my first NodeJs Page</h1>");
+
+  // 如果要返回中文,则需要设置响应头
+  res.writeHead(200,{"Content-Type":"text/html;charset=utf-8"});
+  res.end("中文");
+});
+
+// 3 开启服务,监听端口
+server.listen(4000);
+
